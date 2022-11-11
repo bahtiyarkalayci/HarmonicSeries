73fc241275dc6e5703ade6895e2e47b26fdce06f
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-11 11:04:24 +0300

    https://www.patika.dev/tr

--- src/HarmonicSeries.java
+++ src/HarmonicSeries.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class HarmonicSeries {
+import java.util.Scanner;
+
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        int number;
+        double series=0.0;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("bir sayı giriniz");
+        number=scanner.nextInt();
+
+        for (double i=1; i<=number;i++){
+            series+=(1/i);
+
+
+        }
+        System.out.println("harmonic seri="+series);
+    }
 }
